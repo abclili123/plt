1600bcf6a91496383544fd3461c2aed9353dc47d
abclili123 <lilianaseoror@gmail.com>
2024-10-11 10:51:10 -0400

    structure of lexer

---
+++ lexer.py
@@ -0,0 +1,17 @@
+# first read in file
+
+# read the file one word at a time
+
+# make sure the word is split correctly
+# for example, a token cannot have {, }, or , at the end
+# so seperate out the delimiters
+
+# first try to match delimiters
+
+# then try to match keywords
+
+# then try to match types
+
+# then try to match literals (i think in the order of the grammar)
+
+# catch any errors
