0f954bd40716871b927b3b54b28af47869fc44eb
abclili123 <lilianaseoror@gmail.com>
2024-10-11 10:57:16 -0400

    read input file one token at a time

--- lexer.py
+++ lexer.py
@@ -1,17 +1,36 @@
-# first read in file
+import sys
 
-# read the file one word at a time
+def lexer(input_program):
+    # first read in file
+    try:
+        with open(input_program, 'r') as program_body:
+            # read the file one word at a time
+            for line in program_body:
+                for token in line:
+                    # make sure the word is split correctly
+                    # for example, a token cannot have {, }, or , at the end
+                    # so seperate out the delimiters
 
-# make sure the word is split correctly
-# for example, a token cannot have {, }, or , at the end
-# so seperate out the delimiters
+                    # first try to match delimiters
 
-# first try to match delimiters
+                    # then try to match keywords
 
-# then try to match keywords
+                    # then try to match types
 
-# then try to match types
+                    # then try to match literals (i think in the order of the grammar)
 
-# then try to match literals (i think in the order of the grammar)
+                    # catch any errors
+                    pass
 
-# catch any errors
+    except FileNotFoundError:
+        print(f"Error: The program '{filename}' was not found.")
+    except Exception as e:
+        print(f"Error reading file: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python lexer.py <filename>")
+    else:
+        filename = sys.argv[1]
+        lexer(filename)
+
